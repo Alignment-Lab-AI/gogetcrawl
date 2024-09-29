7894a3f41069bc90b40b4c0738680ebdaa0931ce
Willem de Groot <gwillem@gmail.com>
2024-09-29 10:07:12 +0200

    Loop over older CC indices to match date filters

--- common/common.go
+++ common/common.go
@@ -3,21 +3,23 @@ package common
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"mime"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/corpix/uarand"
 	"github.com/valyala/fasthttp"
 )
 
-var Status503Error = errors.New("Server returned 503 status response")
-var Status500Error = errors.New("Server returned 500 status response. (Slow down)")
+var (
+	Status503Error = errors.New("Server returned 503 status response")
+	Status500Error = errors.New("Server returned 500 status response. (Slow down)")
+)
 
 // WebArchive and Common Crawl (index.commoncrawl.org) CDX API Response structure from
 type CdxResponse struct {
@@ -47,13 +49,13 @@ type Source interface {
 }
 
 type RequestConfig struct {
-	URL            string   // Url to parse
-	Filters        []string // Extenstion to search
-	Limit          uint     // Max number of results per page
-	CollapseColumn string   // Which column to use to collapse results
-	SinglePage     bool     // Get results only from 1st page (mostly used for tests)
-	FromDate       string   // Filter results from Date
-	ToDate         string   // Filter results to Date
+	URL            string    // Url to parse
+	Filters        []string  // Extenstion to search
+	Limit          uint      // Max number of results per page
+	CollapseColumn string    // Which column to use to collapse results
+	SinglePage     bool      // Get results only from 1st page (mostly used for tests)
+	FromDate       time.Time // Filter results from Date
+	ToDate         time.Time // Filter results to Date
 }
 
 // GetUrlFromConfig ... Compose URL with CDX server request parameters
@@ -74,12 +76,12 @@ func (config RequestConfig) GetUrl(serverURL string, page int) string {
 		}
 	}
 
-	if config.FromDate != "" {
-		reqURL = fmt.Sprintf("%v&from=%v", reqURL, config.FromDate)
+	if !config.FromDate.IsZero() {
+		reqURL = fmt.Sprintf("%v&from=%v", reqURL, config.FromDate.Format("20060102"))
 	}
 
-	if config.ToDate != "" {
-		reqURL = fmt.Sprintf("%v&to=%v", reqURL, config.ToDate)
+	if !config.ToDate.IsZero() {
+		reqURL = fmt.Sprintf("%v&to=%v", reqURL, config.ToDate.Format("20060102"))
 	}
 
 	if !config.SinglePage {
@@ -134,28 +136,32 @@ func DoRequest(url string, timeout int, headers map[string]string) ([]byte, erro
 
 // Get ... Performs HTTP GET request and returns response bytes
 func Get(url string, timeout int, maxRetries int) ([]byte, error) {
-	var err error
-	var responseBytes []byte
+	log.Printf("GET request to %s (timeout: %d, retries: %d)", url, timeout, maxRetries)
 
-	for i := maxRetries; i != 0; i-- {
-		log.Printf("GET [t=%v] [r=%v]: %v", timeout, maxRetries, url)
-
-		responseBytes, err = DoRequest(url, timeout, nil)
-		if err == nil {
-			return responseBytes, nil
-		}
-
-		if err == Status503Error || err == Status500Error {
-			time.Sleep(time.Duration(timeout * int(time.Second)))
-		}
+	client := &http.Client{
+		Timeout: time.Duration(timeout) * time.Second,
 	}
 
-	return nil, fmt.Errorf("Perfomed max retries, no result: %v", err)
+	var resp *http.Response
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		log.Printf("GET [t=%v] [r=%v]: %v", timeout, maxRetries, url)
+
+		resp, err = client.Get(url)
+		if err == nil && resp.StatusCode == 200 {
+			break
+		}
+		log.Printf("Attempt %d failed: %v", i+1, err)
+		time.Sleep(time.Second * time.Duration(i+1))
+	}
+	defer resp.Body.Close()
+	return io.ReadAll(resp.Body)
 }
 
 // Save data using file fullpath
 func SaveFile(data []byte, path string) error {
-	err := os.WriteFile(path, data, 0644)
+	err := os.WriteFile(path, data, 0o644)
 	if err != nil {
 		return err
 	}
@@ -167,46 +173,28 @@ func SaveFile(data []byte, path string) error {
 func SaveFiles(results <-chan []*CdxResponse, outputDir string, errors chan error, downloadRate float32) {
 	log.Println("[SaveFiles] worker started:", outputDir)
 
-	for {
-		select {
-		case resBatch, ok := <-results:
-			if ok {
-				for _, res := range resBatch {
-					data, err := res.Source.GetFile(res)
-					if err != nil {
-						errors <- err
-						continue
-					}
-
-					exts, _ := mime.ExtensionsByType(res.MimeType)
-					if exts == nil {
-						exts = []string{""}
-					}
-
-					filename := fmt.Sprintf("%v-%v-%v%v", res.Original, res.Timestamp, res.Source.Name(), exts[0])
-					escapedFilename := url.QueryEscape(filename)
-					fullPath := filepath.Join(outputDir, escapedFilename)
-
-					err = SaveFile(data, fullPath)
-					if err != nil {
-						errors <- err
-					}
-
-					time.Sleep(time.Second * time.Duration(downloadRate))
-				}
+	for resBatch := range results {
+		for _, res := range resBatch {
+			data, err := res.Source.GetFile(res)
+			if err != nil {
+				errors <- err
+				continue
 			}
+
+			exts, err := mime.ExtensionsByType(res.MimeType)
+			if err != nil || len(exts) == 0 {
+				errors <- fmt.Errorf("Cannot get extension from file")
+				continue
+			}
+
+			filename := fmt.Sprintf("%v-%v-%v%v", res.Original, res.Timestamp, res.Source.Name(), exts[0])
+			fullPath := filepath.Join(outputDir, url.QueryEscape(filename))
+
+			if err := SaveFile(data, fullPath); err != nil {
+				errors <- err
+			}
+
+			time.Sleep(time.Duration(downloadRate * float32(time.Second)))
 		}
 	}
-
-}
-
-func GetFileExtenstion(file *[]byte) (string, error) {
-	contentType := http.DetectContentType(*file)
-	contentType = strings.Split(contentType, ";")[0]
-	exts, err := mime.ExtensionsByType(contentType)
-	if err != nil || len(exts) == 0 {
-		return "", fmt.Errorf("Cannot get extension from file")
-	}
-
-	return exts[0], nil
 }
